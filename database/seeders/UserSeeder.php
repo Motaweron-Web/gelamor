2130b25aea238a85ff69fabc3246b8044e535103
osamarafa1994 <osama@motaweron.com>
2022-12-07 13:12:49 +0200

    regiser user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,30 +15,23 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
 
             [
-
-                'name_ar' => 'اسلام محمد',
-                'name_en' => 'Islam Mohammed',
+                'name' => 'Islam Mohammed',
                 'img' => 'default.png',
                 'email' => 'admin@admin.com',
                 'password' => Hash::make('123456'),
                 'phone' => 758584,
-                'location_ar' => 'القاهره',
-                'location_en' => 'cairo street',
+                'location' => 'cairo street',
                 'country_id' => rand(1,2),
             ],
             [
-
-                'name_ar' => 'علي محمد',
                 'name_en' => 'ali mohammed',
                 'img' => 'default.png',
                 'email' => 'admin123@gmail.com',
                 'password' => Hash::make('123456'),
                 'phone' => 9898989999,
-                'location_ar' => 'القاهره',
-                'location_en' => 'cairo street',
+                'location' => 'القاهره',
                 'country_id' => rand(1,2),
             ]
-
 
         ]);
     }
